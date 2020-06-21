e860704b9aa39bebd7799436462107aa91ade8e5
Ali San Gumusay <silvermoon@pop-os.localdomain>
2020-06-21 13:06:32 +0200

    Box Screen

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+function BoxScreen() {
+	return (
+		<View style={styles.parentStyle}>
+			<View style={styles.viewOneStyle}></View>
+			<View style={styles.viewTwoParent}>
+				<View style={styles.viewTwoStyle}></View>
+			</View>
+			<View style={styles.viewThreeStyle}></View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	parentStyle: {
+		borderWidth: 3,
+		borderColor: 'black',
+		height: 200,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+
+	viewOneStyle: {
+		height: 50,
+		width: 50,
+		backgroundColor: 'red',
+	},
+	viewTwoStyle: {
+		height: 50,
+		width: 50,
+		backgroundColor: 'green',
+	},
+	viewTwoParent: {
+		height: 100,
+		justifyContent: 'flex-end',
+	},
+	viewThreeStyle: {
+		height: 50,
+		width: 50,
+		backgroundColor: 'blue',
+	},
+});
+
+export default BoxScreen;
